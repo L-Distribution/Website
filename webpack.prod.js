ea3624cb8e71ee742840918d1d9acdba9e190e1c
Aidan Goettsch <yayes.aidan@gmail.com>
2018-08-27 22:20:05 -0400

    add preact

--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const common = require('./webpack.common.js')
-
-module.exports = merge(common, {
-  mode: 'production',
-  plugins: [new UglifyJSPlugin()]
-})
